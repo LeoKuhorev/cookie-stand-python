f5792ea4a2f38f9a4cc24bf9dc707f75ab5dd358
ticochuck <chucklivilla@gmail.com>
2020-10-21 10:55:51 -0700

    added sales page with basic table

--- cookies/urls.py
+++ cookies/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home
+from .views import home, sales_data
 
 
 urlpatterns = [
     path('', home, name='home'),
+    path('sales/', sales_data, name='sales')
 ]
