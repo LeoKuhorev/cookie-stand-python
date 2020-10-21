b764c3535e9febbba2122320f30913b6b03279e1
Leo Kukharau <kuhorev.leonid@gmail.com>
2020-10-20 20:22:37 -0700

    custom admin added

--- cookies/admin.py
+++ cookies/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Cookie, Location
 
-# Register your models here.
+
+class CookieAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price')
+
+
+class LocationAdmin(admin.ModelAdmin):
+
+    def render_avg_sale(self, obj):
+        return f'${obj.avg_sale}'
+
+    def render_min_customers(self, obj):
+        return f'{obj.min_customers} customers/hour'
+
+    def render_max_customers(self, obj):
+        return f'{obj.max_customers} customers/hour'
+
+    list_display = ('name', 'manager', 'render_min_customers',
+                    'render_max_customers', 'render_avg_sale')
+    list_display_links = ('name', 'manager', 'render_min_customers',
+                          'render_max_customers', 'render_avg_sale')
+
+
+admin.site.register(Cookie, CookieAdmin)
+admin.site.register(Location, LocationAdmin)
--- cookies/models.py
+++ cookies/models.py
@@ -3,8 +3,17 @@ from users.models import CustomUser
 
 class Location(models.Model):
     name = models.CharField(max_length=128)
-    manager = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    manager = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name='Location Manager')
+    min_customers = models.IntegerField(help_text='Enter minimum number of customers')
+    max_customers = models.IntegerField(help_text='Enter maximum number of customers')
+    avg_sale = models.DecimalField(max_digits=5, decimal_places=2, help_text='Enter the average number of cookies per sale')
     
     def __str__(self):
         return self.name
     
+class Cookie(models.Model):
+    name = models.CharField(max_length=128)
+    price = models.DecimalField(decimal_places=2, max_digits=5)
+    
+    def __str__(self):
+        return self.name
